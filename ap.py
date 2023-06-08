40a7f884896b714c69e3e075abffc562f4722e6b
Luiz Gustavo S Lopes <lglopes1789@gmail.com>
2023-06-08 13:00:02 -0300

    v5

--- ap.py
+++ ap.py
@@ -1,60 +1,89 @@
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
-import threading
+from flask_socketio import SocketIO, emit, send
+import threading, time, requests, os
+import signal
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+app.config['SECRET_KEY'] = 'perinbocadaparafuseta!'
 socketio = SocketIO(app)
+messages = []
+clients = set()  # Conjunto para armazenar os clientes conectados
 
-# Dicionário de threads dos usuários
-user_threads = {}
+exe = True
+
+
+#funaçao pagina
+@app.route('/')
+def index():
+    server = '2'
+    return render_template('index.html')
 
 
 @socketio.on('connect')
-def handle_connect():
-    # Cria uma nova thread para processar a conexão do cliente
-    thread = threading.Thread(target=handle_client)
-    thread.start()
+def connect_handler():
+    global clients
+    clients.add(request.sid)  # Adiciona o socket ID do cliente à lista de clientes conectados
+    print(f'Cliente conectado: {request.sid}')
+    socketio.emit('update_server_number', {'num_server': 2})
+    # Outras ações que você deseja realizar quando um cliente se conecta
 
 
 @socketio.on('disconnect')
-def handle_disconnect():
-    # Remove o usuário da lista de usuários conectados e finaliza a thread correspondente
-    user_id = request.sid
-    user_threads[user_id]['running'] = False
-    del user_threads[user_id]
+def disconnect_handler():
+    global clients
+    clients.remove(request.sid)  # Remove o socket ID do cliente da lista de clientes conectados
+    print(f'Cliente desconectado: {request.sid}')
+    # Outras ações que você deseja realizar quando um cliente se desconecta
+
+
+@socketio.on('sendMessage')
+def send_message_handler(msg):
+    global stop_server, exe
+    if msg["message"] == 'stop':
+        off()
+    salvar = f'{msg["name"]},, {msg["message"]}'
+    # Salvar mensagem em um arquivo de texto
+    with open('mensagens.txt', 'a') as file:
+        file.write(salvar + '\n')
+    emit('getMessage', msg, broadcast=True)
 
 
 @socketio.on('message')
-def handle_message(data):
-    user_id = request.sid
-    user_threads[user_id]['message'] = data['message']
+def message_handler(msg):
+    messages = []
+    with open('mensagens.txt', 'r') as file:
+        while True:
+            a = file.readline().rstrip('\n')
+            if a == '':
+                break
+            b = a.split(',,')
+            obj = {'name': b[0], 'message': b[1]}
+            messages.append(obj)
+    send(messages)
 
 
-def handle_client():
-    user_id = request.sid
-
-    # Cria uma nova thread para cuidar do usuário
-    thread = threading.Thread(target=handle_user, args=(user_id,))
-    user_threads[user_id] = {'thread': thread, 'message': None, 'running': True}
-    thread.start()
-
-
-def handle_user(user_id):
-    while user_threads[user_id]['running']:
-        # Verifica se há uma nova mensagem para o usuário
-        if user_threads[user_id]['message']:
-            message = user_threads[user_id]['message']
-            emit('message', {'user_id': user_id, 'message': message}, broadcast=True)
-            user_threads[user_id]['message'] = None
-
-        socketio.sleep(1)  # Aguarda 1 segundo antes de verificar novamente
+#funçao servidor
+def check_connection(url):
+    try:
+        response = requests.get(url)
+        print(response)
+        return response.status_code == 200
+    except requests.exceptions.RequestException:
+        return False
 
 
-@app.route('/')
-def index():
-    return render_template('index1.html')
+def off():
+    global exe
+    exe = True
+    os.kill(os.getpid(), signal.SIGINT)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    while exe:
+        time.sleep(1)
+        exe = check_connection('http://192.168.2.104:5000/')
+        print(exe)
+        pass
+    socketio.run(app, host='192.168.2.104', port=5000)
+
--- app.py
+++ app.py
@@ -1,28 +1,33 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, send
-import eventlet
 import threading
+import os
+import signal
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'perinbocadaparafuseta!'
-io = SocketIO(app, async_mode='eventlet')
-
+socketio = SocketIO(app)
 messages = []
 clients = set()  # Conjunto para armazenar os clientes conectados
 
-
 @app.route('/')
 def home():
-    return render_template("index.html")
 
-@io.on('connect')
+    server = '1'
+    return render_template('index.html')
+
+
+@socketio.on('connect')
 def connect_handler():
     global clients
     clients.add(request.sid)  # Adiciona o socket ID do cliente à lista de clientes conectados
     print(f'Cliente conectado: {request.sid}')
-    # Outras ações que você deseja realizar quando um cliente se conecta
+    socketio.emit('update_server_number', {'num_server': 1})
 
-@io.on('disconnect')
+
+
+@socketio.on('disconnect')
 def disconnect_handler():
     global clients
     clients.remove(request.sid)  # Remove o socket ID do cliente da lista de clientes conectados
@@ -30,15 +35,19 @@ def disconnect_handler():
     # Outras ações que você deseja realizar quando um cliente se desconecta
 
 
-@io.on('sendMessage')
+@socketio.on('sendMessage')
 def send_message_handler(msg):
+    global stop_server, exe
+    if msg["message"] == 'stop':
+        off()
     salvar = f'{msg["name"]},, {msg["message"]}'
     # Salvar mensagem em um arquivo de texto
     with open('mensagens.txt', 'a') as file:
         file.write(salvar + '\n')
     emit('getMessage', msg, broadcast=True)
 
-@io.on('message')
+
+@socketio.on('message')
 def message_handler(msg):
     messages = []
     with open('mensagens.txt', 'r') as file:
@@ -51,6 +60,16 @@ def message_handler(msg):
             messages.append(obj)
     send(messages)
 
+def off():
+    global exe
+    exe = True
+    os.kill(os.getpid(), signal.SIGINT)
+
 
 if __name__ == '__main__':
-    io.run(app)
+    #server_thread = threading.Thread(target=stop_server_thread)
+    #server_thread.start()
+    socketio.run(app, debug=True, host='192.168.2.104', port=5000)
+    #server_thread.join()
+
+
